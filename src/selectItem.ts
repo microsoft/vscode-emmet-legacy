2c083890c2a36f9ac4989d579da5be215ed5b6aa
Ramya Achutha Rao <ramyar@microsoft.com>
2017-05-22 15:09:04 -0700

    Select next/prev html attribute

--- src/selectItem.ts
+++ src/selectItem.ts
@@ -12,19 +12,22 @@ export function fetchSelectItem(direction: string): void {
     let rootNode: Node = parse(editor.document.getText());
     let newSelections: vscode.Selection[] = [];
     editor.selections.forEach(selection => {
-        let updatedSelection = direction === 'next' ? nextItem(selection.active, editor, rootNode) : prevItem(selection.active, editor, rootNode);
+        let updatedSelection = direction === 'next' ? nextItem(selection, editor, rootNode) : prevItem(selection, editor, rootNode);
         newSelections.push(updatedSelection ? updatedSelection : selection);
     });
     editor.selections = newSelections;
 }
 
-function nextItem(position: vscode.Position, editor: vscode.TextEditor, rootNode: Node): vscode.Selection {
-    let offset = editor.document.offsetAt(position);
+function nextItem(selection: vscode.Selection, editor: vscode.TextEditor, rootNode: Node): vscode.Selection {
+    let offset = editor.document.offsetAt(selection.active);
     let currentNode = getNode(rootNode, offset);
 
-    // Cursor is in the open tag
+    // Cursor is in the open tag, look for attributes
     if (offset < currentNode.open.end) {
-        // TODO: Check attributes
+        let attrSelection = getNextAttribute(selection, editor.document, currentNode);
+        if (attrSelection) {
+            return attrSelection;
+        }
     }
 
     // Get the first child of current node which is right after the cursor
@@ -42,19 +45,18 @@ function nextItem(position: vscode.Position, editor: vscode.TextEditor, rootNode
     return getSelectionFromNode(nextNode, editor.document);
 }
 
-function prevItem(position: vscode.Position, editor: vscode.TextEditor, rootNode: Node): vscode.Selection {
-    let offset = editor.document.offsetAt(position);
+function prevItem(selection: vscode.Selection, editor: vscode.TextEditor, rootNode: Node): vscode.Selection {
+    let offset = editor.document.offsetAt(selection.active);
     let currentNode = getNode(rootNode, offset);
     let prevNode: Node;
 
     // Cursor is in the open tag after the tag name
     if (offset > currentNode.open.start + currentNode.name.length + 1 && offset <= currentNode.open.end) {
-        // TODO: Check attributes
-        return getSelectionFromNode(currentNode, editor.document);
+        prevNode = currentNode;
     }
 
     // Cursor is inside the tag
-    if (offset > currentNode.open.end) {
+    if (!prevNode && offset > currentNode.open.end) {
         if (!currentNode.firstChild) {
             // No children, so current node should be selected
             prevNode = currentNode
@@ -76,7 +78,8 @@ function prevItem(position: vscode.Position, editor: vscode.TextEditor, rootNode
         prevNode = currentNode.parent;
     }
 
-    return getSelectionFromNode(prevNode, editor.document);
+    let attrSelection = getPrevAttribute(selection, editor.document, prevNode);
+    return attrSelection ? attrSelection : getSelectionFromNode(prevNode, editor.document);
 }
 
 function getSelectionFromNode(node: Node, document: vscode.TextDocument): vscode.Selection {
@@ -85,5 +88,53 @@ function getSelectionFromNode(node: Node, document: vscode.TextDocument): vscode
         let selectionEnd = node.type === 'comment' ? document.positionAt(node.open.end - 1) : selectionStart.translate(0, node.name.length);
 
         return new vscode.Selection(selectionStart, selectionEnd);
+    }
+}
+
+function getNextAttribute(selection: vscode.Selection, document: vscode.TextDocument, node: Node): vscode.Selection {
+
+    if (!node.attributes || node.attributes.length === 0 || node.type === 'comment') {
+        return;
+    }
+
+    let selectionStart = document.offsetAt(selection.anchor);
+    let selectionEnd = document.offsetAt(selection.active);
+
+    for (let i = 0; i < node.attributes.length; i++) {
+        let attr = node.attributes[i];
+
+        if (selectionEnd < attr.start) {
+            // select full attr
+            return new vscode.Selection(document.positionAt(attr.start), document.positionAt(attr.end));
+        }
+
+        if ((attr.value.start !== attr.value.end) && ((selectionStart === attr.start && selectionEnd === attr.end) || selectionEnd < attr.end - 1)) {
+            // select attr value
+            return new vscode.Selection(document.positionAt(attr.value.start), document.positionAt(attr.value.end));
+        }
+    }
+}
+
+function getPrevAttribute(selection: vscode.Selection, document: vscode.TextDocument, node: Node): vscode.Selection {
+
+    if (!node.attributes || node.attributes.length === 0 || node.type === 'comment') {
+        return;
+    }
+
+    let selectionStart = document.offsetAt(selection.anchor);
+    let selectionEnd = document.offsetAt(selection.active);
+
+    for (let i = node.attributes.length - 1; i >= 0; i--) {
+        let attr = node.attributes[i];
+
+        if (selectionStart > attr.value.start) {
+            // select attr value
+            return new vscode.Selection(document.positionAt(attr.value.start), document.positionAt(attr.value.end));
+        }
+
+        if (selectionStart > attr.start) {
+            // select full attr
+            return new vscode.Selection(document.positionAt(attr.start), document.positionAt(attr.end));
+        }
     }
 }
